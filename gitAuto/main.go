da7d0e671a494c0ae4613ec0e5d9509e0deb3428
NATALI-LEV <natalillev@gmail.com>
2023-11-12 10:28:47 +0200

    automated commit

--- gitAuto/main.go
+++ gitAuto/main.go
@@ -21,32 +21,46 @@ func updateCommitPush() {
 		os.Exit(1)
 	}
 
-	// Command 2: Commit all changes with a predefined message
-	commitCommand := exec.Command("git", "commit", "-m", "automated commit")
-	commitCommand.Stdout = os.Stdout
-	commitCommand.Stderr = os.Stderr
-
-	// Run the 'git commit' command
-	if err := commitCommand.Run(); err != nil {
-		// Print an error message and exit if the command fails
-		fmt.Fprintln(os.Stderr, "Error: Failed to commit changes.")
+	// Command 2: Check if there are changes to commit
+	statusCommand := exec.Command("git", "status", "--porcelain")
+	statusOutput, err := statusCommand.Output()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error: Failed to check git status.")
 		os.Exit(1)
 	}
 
-	// Command 3: Push to remote (origin master)
-	pushCommand := exec.Command("git", "push", "origin", "master")
-	pushCommand.Stdout = os.Stdout
-	pushCommand.Stderr = os.Stderr
+	// If there are changes, proceed with commit and push
+	if len(statusOutput) > 0 {
+		// Command 3: Commit all changes with a predefined message
+		commitCommand := exec.Command("git", "commit", "-m", "automated commit")
+		commitCommand.Stdout = os.Stdout
+		commitCommand.Stderr = os.Stderr
 
-	// Run the 'git push' command
-	if err := pushCommand.Run(); err != nil {
-		// Print an error message and exit if the command fails
-		fmt.Fprintln(os.Stderr, "Error: Failed to push changes to remote.")
-		os.Exit(1)
+		// Run the 'git commit' command
+		if err := commitCommand.Run(); err != nil {
+			// Print an error message and exit if the command fails
+			fmt.Fprintln(os.Stderr, "Error: Failed to commit changes.")
+			os.Exit(1)
+		}
+
+		// Command 4: Push to remote (origin main)
+		pushCommand := exec.Command("git", "push", "origin", "main")
+		pushCommand.Stdout = os.Stdout
+		pushCommand.Stderr = os.Stderr
+
+		// Run the 'git push' command
+		if err := pushCommand.Run(); err != nil {
+			// Print an error message and exit if the command fails
+			fmt.Fprintln(os.Stderr, "Error: Failed to push changes to remote.")
+			os.Exit(1)
+		}
+
+		// Print a success message if all commands are successful
+		fmt.Println("Successfully added, committed, and pushed changes!")
+	} else {
+		// Print a message if there are no changes to commit
+		fmt.Println("No changes to commit.")
 	}
-
-	// Print a success message if all commands are successful
-	fmt.Println("Successfully added, committed, and pushed changes!")
 }
 
 func main() {
